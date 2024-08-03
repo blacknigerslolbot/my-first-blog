5b0ce745653425dd83dfb2f4417b2f0df9be4166
Nigga <inalovsajfula@gmail.com>
2024-08-03 11:19:03 +0300

    MySite version2. Add templates and first urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(reguest):
+    return render(reguest, 'blog/index.html', {})
+
+def categ(reguest):
+    return HttpResponse('<h1>page1</h1>')
